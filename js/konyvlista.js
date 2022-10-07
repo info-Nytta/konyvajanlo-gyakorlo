7dda37b7e49f89bb559162aaaabbe1959758d4e7
info-Nytta <58329400+info-Nytta@users.noreply.github.com>
2022-10-07 23:33:47 +0200

    galéria

--- js/konyvlista.js
+++ js/konyvlista.js
@@ -1,19 +1,6 @@
-function listazas() {
-		/*
-		for (i=0; i<konyvek.length; i++)
-		document.write(""+
-    "<div class='konyv'>"+
-			"<img src='img/"+konyvek[i].kep+"'>"+
-			"<h1>"+konyvek[i].iro+"</h1>"+
-			"<h2>"+konyvek[i].cim+"</h2>"+
-			"<h3>"+konyvek[i].kiado+", "+konyvek[i].kiadas+"</h3>"+
-			"<div class='elvalaszto'></div>"+
-			"<p>"+konyvek[i].leiras+"</p>"+
-    "</div>")
-		*/
-		
-		for (k of konyvek)
-		document.write(""+
+function egyKonyv(i){
+	let k=konyvek[i];
+	document.getElementById("konyvinfo").innerHTML=""+
     "<div class='konyv'>"+
 			"<div>"+
 				"<img src='img/"+k.kep+"'>"+
@@ -22,5 +9,44 @@ function listazas() {
 				"<h3>"+k.kiado+", "+k.kiadas+"</h3>"+
 			"</div>"+
 			"<p>"+k.leiras+"</p>"+
-    "</div>")
+    "</div>"
 }
+
+function index(x1,x2) {
+	if (x1+x2<0) return konyvek.length-1;
+	else if (x1+x2>=konyvek.length) return (x1+x2)-konyvek.length;
+	else return x1+x2;
+}
+
+function kepdb() {
+	let kepszel=120;
+	let keretszel=700;
+	if (screen.width>keretszel) return parseInt(keretszel/kepszel-1);
+	else return parseInt(screen.width/kepszel-2);
+}
+
+function mozgas(mennyit){
+	aktKonyv=index(aktKonyv,mennyit);
+	let kI=aktKonyv;
+	let kiKepek="";
+	for (i=0; i<kepdb(); i++) {
+		kI=index(aktKonyv,i);
+		kiKepek+="<img onclick='egyKonyv("+kI+")' class='slide-kep' id='' src='img/"+konyvek[kI].kep+"'>";
+	}
+	document.getElementById("kepek").innerHTML=kiKepek;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
